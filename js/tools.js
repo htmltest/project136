daaaff6bf748fe88bce068b10a6942282576d9f3
htmltest <struev.vs@gmail.com>
2019-08-15 15:29:43 +0300

    updates
    доработана страница продукта - добавлен зум
    добавлены страницы новостей, одной новости, регистрации оптовика

--- js/tools.js
+++ js/tools.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
 
+    $.validator.addMethod('phoneRU',
+        function(phone_number, element) {
+            return this.optional(element) || phone_number.match(/^\+7 \(\d{3}\) \d{3}\-\d{2}\-\d{2}$/);
+        },
+        'Ошибка заполнения'
+    );
+
+    $('body').on('change', '.form-file input', function() {
+        var curInput = $(this);
+        var curField = curInput.parents().filter('.form-file');
+        var curForm = curField.parents().filter('form');
+        var curName = curInput.val().replace(/.*(\/|\\)/, '');
+        var curNameArray = curName.split('.');
+        var curExt = curNameArray[curNameArray.length - 1];
+        curNameArray.pop();
+        var curNameText = curNameArray.join('.');
+        if (curNameText.length > 10) {
+            curNameText = curNameText.substring(0, 10) + '...' + curNameText.slice(-1);
+        }
+        curField.find('.form-file-name-text').html(curNameText + '.' + curExt);
+        curForm.find('.form-files').append(curForm.data('filesCode'));
+    });
+
+    $('form').each(function() {
+        initForm($(this));
+    });
+
+    $('body').on('click', '.form-file-name-remove', function() {
+        var curField = $(this).parents().filter('.form-file');
+        curField.remove();
+    });
+
     $('.menu-catalogue-link').click(function(e) {
         $('html').toggleClass('menu-open');
         e.preventDefault();
@@ -84,6 +116,14 @@ $(document).ready(function() {
         e.preventDefault();
     });
 
+    $('.product-gallery-big-item img').each(function() {
+        new Drift(this, {
+            paneContainer: $('.product-info')[0],
+            containInline: true,
+            hoverBoundingBox: true
+        });
+    });
+
 });
 
 $(window).on('load resize', function() {
@@ -114,4 +154,99 @@ function resizeCatalogue() {
             });
         });
     });
+}
+
+function initForm(curForm) {
+    curForm.find('input.phoneRU').mask('+7 (000) 000-00-00');
+
+    if (curForm.find('.form-files').length > 0) {
+        curForm.data('filesCode', curForm.find('.form-files').html());
+    }
+
+    curForm.validate({
+        ignore: '',
+        submitHandler: function(form) {
+            if ($(form).hasClass('ajax-form')) {
+                var formData = new FormData(form);
+
+                if ($(form).find('[type=file]').length != 0) {
+                    var file = $(form).find('[type=file]')[0].files[0];
+                    formData.append('file', file);
+                }
+
+                windowOpen($(form).attr('action'), formData);
+            } else {
+                form.submit();
+            }
+        }
+    });
+}
+
+function windowOpen(linkWindow, dataWindow) {
+    if ($('html').hasClass('menu-open')) {
+        $('.wrapper').css({'top': 'auto'});
+        $('html').removeClass('menu-open');
+        $(window).scrollTop($('.wrapper').data('curScroll'));
+    }
+
+    if ($('.window').length > 0) {
+        windowClose();
+    }
+
+    var curPadding = $('.wrapper').width();
+    var curWidth = $(window).width();
+    if (curWidth < 480) {
+        curWidth = 480;
+    }
+    var curScroll = $(window).scrollTop();
+    $('html').addClass('window-open');
+    curPadding = $('.wrapper').width() - curPadding;
+    $('body').css({'margin-right': curPadding + 'px'});
+    $('body').append('<div class="window"><div class="window-loading"></div></div>')
+    $('.wrapper').css({'top': -curScroll});
+    $('.wrapper').data('curScroll', curScroll);
+    $('meta[name="viewport"]').attr('content', 'width=' + curWidth);
+
+    $.ajax({
+        type: 'POST',
+        url: linkWindow,
+        processData: false,
+        contentType: false,
+        dataType: 'html',
+        data: dataWindow,
+        cache: false
+    }).done(function(html) {
+        $('.window').append('<div class="window-container window-container-preload"><div class="window-content">' + html + '<a href="#" class="window-close"></a></div></div>')
+
+        windowPosition();
+
+        $('.window-container-preload').removeClass('window-container-preload');
+
+        $('.window form').each(function() {
+            initForm($(this));
+        });
+
+    });
+}
+
+function windowPosition() {
+    if ($('.window').length > 0) {
+        $('.window-container').css({'left': '50%', 'margin-left': -$('.window-container').width() / 2});
+
+        $('.window-container').css({'top': '50%', 'margin-top': -$('.window-container').height() / 2, 'padding-bottom': 0});
+        if ($('.window-container').height() > $('.window').height() - 80) {
+            $('.window-container').css({'top': 40, 'margin-top': 0, 'padding-bottom': 40});
+        }
+    }
+}
+
+function windowClose() {
+    if ($('.window').length > 0) {
+        $('.window').remove();
+        $('html').removeClass('window-open');
+        $('body').css({'margin-right': 0});
+        $('.wrapper').css({'top': 0});
+        $(window).scrollTop($('.wrapper').data('curScroll'));
+        $('meta[name="viewport"]').attr('content', 'width=device-width');
+    }
 }
